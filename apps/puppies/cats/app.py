1f98e05291ea3216cee7d6e1f874c5f2973f3d2f
Alfredo Zavala Sulca <azavala@belcorp.biz>
2018-09-29 01:29:36 -0500

    first commit

---
+++ apps/puppies/cats/app.py
@@ -0,0 +1,20 @@
+import os
+import random
+import json
+from flask import Flask, render_template
+
+
+with open('cats.json') as data_file:
+    quotes = json.load(data_file)
+
+app = Flask(__name__)
+
+
+@app.route('/cats')
+def index():
+    quote = quotes[random.randrange(len(quotes))]
+    return render_template('index.html', quote=quote['quote'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
